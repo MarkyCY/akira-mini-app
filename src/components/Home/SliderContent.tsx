397fb1eea462034ffa0159671db3f143f655f361
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-09-12 20:34:17 -0400

    Slider Card

---
+++ src/components/Home/SliderContent.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import SliderCard from './SliderCard';
+import { Suspense } from 'react';
+import SliderCardSkeleton from './SliderCardSkeleton';
+
+const customStyles: React.CSSProperties = {
+    position: 'relative',
+    '--radix-scroll-area-corner-width': '0px',
+    '--radix-scroll-area-corner-height': '0px'
+} as React.CSSProperties;
+
+export default function SliderContent() {
+
+    return (
+        <div className='relative overflow-hidden' style={customStyles}>
+            <div data-radix-scroll-area-viewport="" className="h-full w-full rounded-[inherit]" style={{ overflow: 'auto' }}>
+                <div style={{ minWidth: '100%', display: 'table' }}>
+                    <div className="flex space-x-4">
+                        <Suspense fallback={<SliderCardSkeleton />}>
+                            <SliderCard />
+                        </Suspense>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
