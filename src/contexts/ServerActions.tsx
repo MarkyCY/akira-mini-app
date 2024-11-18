7ffdf4e65fb3c3e85e4ec29ac7b03ebe9b4ccf80
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-11-18 13:08:24 -0500

    Add ServerActions to Auth and get Users

---
+++ src/contexts/ServerActions.tsx
@@ -0,0 +1,53 @@
+"use server";
+
+export const fetchUser = async (token: string) => {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+  try {
+    const response = await fetch(`${API_URL}/users/me/`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Accept': 'application/json',
+      },
+    });
+    if (!response.ok) {
+        if (response.status === 401) {
+            throw new Error('Credenciales incorrectas');
+        } else {
+            throw new Error('Error al obtener los datos');
+        }
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const login = async (username: string, password: string) => {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+  try {
+    const response = await fetch(`${API_URL}/token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+      },
+      body: `grant_type=password&username=${username}&password=${password}`,
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      return { success: false, message: errorData.detail || 'Error en la autenticación' };
+    }
+
+    const data = await response.json();
+    return data.access_token;
+  } catch (error) {
+    console.error('Error al iniciar sesión:', error);
+    throw error;
+  }
+};
