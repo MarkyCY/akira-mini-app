1e1d04e001487655c8b34491110cc3f5f9773ca8
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-11-18 13:07:23 -0500

    Clean code

--- src/components/stats/UserView.tsx
+++ src/components/stats/UserView.tsx
@@ -1,8 +1,7 @@
 import Image from 'next/image';
 import { TopUser } from '@/lib/StatsDaily';
-import Link from 'next/link';
 import WebApp from "@twa-dev/sdk";
-
+import { useState } from 'react';
 interface UserViewProps {
     user: TopUser;
 }
@@ -11,35 +10,24 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Usa la interfaz en tu componente
 export default function UserView({ user }: UserViewProps) {
+    const [urls, setUrls] = useState(`${API_URL}/user/photo/${user.user_id}`);
+
+    const handleError = () => {
+        setUrls("/unknow.webp");
+      };
 
     return (
-        // <Link href={`/user/${user.user_id}`} className='flex items-center gap-3'>
-        //     <Image 
-        //     className="bg-gradient-to-r from-orange-400 to-pink-500 rounded-full w-10 h-10" 
-        //     src={`${API_URL}/user/photo/${user.user_id}`}
-        //     alt={user.first_name} 
-        //     width={640} 
-        //     height={640} 
-        //     unoptimized={true}
-        //     priority
-        //     />
-        //     <div className="dark:text-white">
-        //         <div className='font-medium'>{user.first_name}</div>
-        //         <div className="text-xs text-gray-400 dark:text-neutral-500">
-        //             {user.messages} mensajes, {user.avg_chars} caracteres por mensaje
-        //         </div>
-        //     </div>
-        // </Link>
         (typeof window !== 'undefined' ? (
             <button onClick={() => WebApp.showAlert("🚧 En desarrollo")} className='flex items-center gap-3'>
                 <Image
                     className="bg-gradient-to-r from-orange-400 to-pink-500 rounded-full w-10 h-10"
-                    src={`${API_URL}/user/photo/${user.user_id}`}
+                    src={urls}
                     alt={user.first_name}
                     width={640}
                     height={640}
                     unoptimized={true}
                     priority
+                    onError={() => handleError()}
                 />
                 <div className="dark:text-white">
                     <div className='font-medium text-left'>{user.first_name}</div>
@@ -48,25 +36,6 @@ export default function UserView({ user }: UserViewProps) {
                     </div>
                 </div>
             </button>
-        ) : (
-            <Link href={`/user/${user.user_id}`} className='flex items-center gap-3'>
-                <Image
-                    className="bg-gradient-to-r from-orange-400 to-pink-500 rounded-full w-10 h-10"
-                    src={`${API_URL}/user/photo/${user.user_id}`}
-                    alt={user.first_name}
-                    width={640}
-                    height={640}
-                    unoptimized={true}
-                    priority
-                />
-                <div className="dark:text-white">
-                    <div className='font-medium'>{user.first_name}</div>
-                    <div className="text-xs text-gray-400 dark:text-neutral-500">
-                        {user.messages} mensajes, {user.avg_chars} caracteres por mensaje
-                    </div>
-                </div>
-            </Link>
-        )
-        )
+        ) : null )
     )
 }
