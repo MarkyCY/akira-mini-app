eb52c242b24a1f0722331af7ab8b67a191c1f3f2
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2025-11-09 12:45:23 -0500

    Ups

--- src/components/Home/Profile/MyAnimeList/refreshTokenMal.tsx
+++ src/components/Home/Profile/MyAnimeList/refreshTokenMal.tsx
@@ -1,11 +1,13 @@
 'use server';
 
+const MAL_BASIC_AUTH = process.env.MAL_BASIC_AUTH as string;
 
 export async function refreshTokenMal(refreshToken: string) {
     const response = await fetch('https://myanimelist.net/v1/oauth2/token', {
         method: 'POST',
+        
         headers: {
-            'Authorization': 'Basic NThkOGUyYjVmNDA3Zjk3OWVjMTFmMDkwZDdjODVhZmQ6ZjhiMjU3YmExNjc0ZTg3MGY4MjI5NzFlMWM5YjFkNzE1YWUzOWYwYzZjM2IwOTI0NTAzZTUzZTcxOTIyYTU3OQ==',
+            'Authorization': `Basic ${MAL_BASIC_AUTH}`,
             'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: new URLSearchParams({
