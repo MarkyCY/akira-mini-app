7bbe13b10e0891de0b63508b30103a0f50f37f76
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2025-11-22 15:46:51 -0500

    Fix page user

--- src/app/user/[user_id]/page.tsx
+++ src/app/user/[user_id]/page.tsx
@@ -1,13 +1,19 @@
 import ShowUser from "@/components/Home/User/showUser";
 
+type PageProps = {
+    params: Promise<{ user_id: string }>;
+};
 
-export default async function UserPage({ params }: { params: { user_id: number } }) {
-    const { user_id } = params;
+export default async function UserPage({ params }: PageProps) {
+    const resolvedParams = await params;
+    const { user_id } = resolvedParams;
+
+    const userIdNumber = Number(user_id);
 
     return (
         <div className="w-full h-auto max-w-sm bg-white rounded-lg dark:bg-neutral-950">
             <div className="w-full max-w-2xl">
-                <ShowUser user_id={user_id} />
+                <ShowUser user_id={userIdNumber} />
             </div>
         </div>
     );
